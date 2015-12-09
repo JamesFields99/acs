6fc37197395493b0cf2d9ba8266f646f688f820e
James Fields <jamesfields@live.com>
2015-12-09 17:49:13 +0000

    Added contact page to nav bar

--- db/migrate/20151207151717_create_contacts.rb
+++ db/migrate/20151207151717_create_contacts.rb
@@ -1,10 +1,10 @@
-class CreateContacts < ActiveRecord::Migration
+class CreateContacts < ActiveRecord::Migration 
   def change
     create_table :contacts do |t|
       t.string :name
       t.string :eamil
       t.text :comments
-      
+
       t.timestamps
     end
   end
